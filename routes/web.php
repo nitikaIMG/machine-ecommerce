b0ce41a4e93783a6a040dde93f7979b178e603f7
nitikaIMG <nitika.img@gmail.com>
2023-02-15 11:36:37 +0530

    first commit

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Seller\VendorAuthController;
+use App\Http\Controllers\Customer\HomePageController;
+use App\Http\Controllers\PushNotifications;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
